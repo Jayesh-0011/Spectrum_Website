c9da9344d5e26a8345e40e402132c1fa52e3b1de
jayesh-0011 <jayesh.sulakshane@gmail.com>
2025-09-01 22:08:12 +0530

    Final

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
   return (
     <nav className=" navbar fixed mx-[55px] top-[20px] z-100 text-white rounded-[12px] shadow-md bg-[#1e1e1e] flex justify-between items-center px-[24px] py-[12px] w-[90%]  hover:bg-[#252525] hover:shadow-lg">
-      <div className="text-[2rem] font-bold">Spectrum</div>
+      <div className="text-[2rem] font-bold hover:cursor-pointer hover:text-[#ff9800]" onClick={() => scrollToArticles('hero')}>Spectrum</div>
       <ul className="flex gap-[2rem] text-[1.2rem] list-none">
         <li className='cursor-pointer hover:text-[#ff9800]' onClick={() => scrollToArticles('about')}>About Us</li>
         <li className='cursor-pointer hover:text-[#ff9800]' onClick={() => scrollToArticles('articles')}>Articles</li>
